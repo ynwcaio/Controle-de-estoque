c4dd5e023a5fa86253e302dcd06c5cd748ea4929
ynwcaio <caiogoncalvesp@gmail.com>
2026-04-11 20:17:57 -0300

    Adcionando o H2 e os inserts

---
+++ products/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_products (product, type, quantity, price) VALUES ('Notebook Dell', 'Eletrônico', 15, 3599.99);
+INSERT INTO tb_products (product, type, quantity, price) VALUES ('Mouse Logitech', 'Eletrônico', 100, 129.90);
+INSERT INTO tb_products (product, type, quantity, price) VALUES ('Cadeira Gamer', 'Móvel', 8, 1299.00);
+INSERT INTO tb_products (product, type, quantity, price) VALUES ('Teclado Mecânico', 'Eletrônico', 30, 349.90);
+INSERT INTO tb_products (product, type, quantity, price) VALUES ('Monitor 24pol', 'Eletrônico', 20, 899.00);
